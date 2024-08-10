5a728d11800ccf5811359bb4fc75e343f70c33d7
Pramod Shrestha <itsmepramod90@gmail.com>
2024-08-10 21:44:35 +0545

    added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import {resolve} from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
    base: '/',
+   resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
+  },
   plugins: [react({
     jsxRuntime: 'classic' 
   })],
